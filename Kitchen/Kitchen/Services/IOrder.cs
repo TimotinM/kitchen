06e352eb844ab0df4af6c8f6a9c54785a836d5cc
Timotin Mihai <mihaitimotin@gmail.com>
2021-09-26 22:29:48 +0300

    create server and Docker file

---
+++ Kitchen/Kitchen/Services/IOrder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kitchen.Services
+{
+     interface IOrder
+     {
+          string Order();
+     }
+}
